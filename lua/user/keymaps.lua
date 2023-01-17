388cc4448969ec84ed323a31b4f04fdec87bd2d1
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 19:34:00 +0800

    chore: add mapping for lsp saga

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -73,9 +73,59 @@ keymap("n", "<leader>fw", function()
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
 -- Diagnostic keymaps
-keymap("n", "[d", vim.diagnostic.goto_prev)
-keymap("n", "]d", vim.diagnostic.goto_next)
 keymap("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Lspsaga
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+-- Code action
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename
+keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+-- Peek Definition
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+
+-- Go to Definition
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Show line diagnostics you can pass argument ++unfocus to make
+-- show_line_diagnostics float window unfocus
+keymap("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+
+-- Show cursor diagnostic
+-- also like show_line_diagnostics  support pass ++unfocus
+keymap("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostic
+keymap("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+-- Diagnostic jump can use `<c-o>` to jump back
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+-- Diagnostic jump with filter like Only jump to error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic").goto_prev { severity = vim.diagnostic.severity.ERROR }
+end)
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic").goto_next { severity = vim.diagnostic.severity.ERROR }
+end)
+
+-- Toggle Outline
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Hover Doc
+-- if there has no hover will have a notify no information available
+-- to disable it just Lspsaga hover_doc ++quiet
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+
+-- Callhierarchy
+keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 
 -- Copilot
 keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
