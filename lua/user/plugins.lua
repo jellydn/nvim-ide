d3c159a56c867e098613465b4a7e43282ec47aab
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 21:47:58 +0800

    feat: add rust tool

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -136,6 +136,9 @@ return packer.startup(function(use)
   use { "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" }
   use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
 
+  -- Rust
+  use { "simrat39/rust-tools.nvim" }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
