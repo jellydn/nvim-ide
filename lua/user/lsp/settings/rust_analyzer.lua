7b0c42ee540120bce1f0ac94a7acdef43752dff6
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 22:04:35 +0800

    feat: add lsp for rust

---
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -0,0 +1,18 @@
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+-- import rust_tools
+local rust_tools = require "rust-tools"
+
+return {
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+}
