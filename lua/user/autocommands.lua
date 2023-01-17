3f38d0ace26c2c68e874946dc3ed8bd20c952d16
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 11:38:44 +0800

    feat: open file form last left off position

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -57,3 +57,14 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     end
   end,
 })
+
+-- Open a file from its last left off position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    if not vim.fn.expand("%:p"):match ".git" and vim.fn.line "'\"" > 1 and vim.fn.line "'\"" <= vim.fn.line "$" then
+      vim.cmd "normal! g'\""
+      vim.cmd "normal zz"
+    end
+  end,
+})
+
