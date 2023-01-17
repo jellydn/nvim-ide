a1903c011ca1704faad8b94142413e143a4436cd
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 10:29:08 +0800

    chore: change dashboard text to itman

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,14 @@ end
 
 local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[                                                        ]],
+  [[      ██╗████████╗    ███╗   ███╗ █████╗ ███╗   ██╗     ]],
+  [[      ██║╚══██╔══╝    ████╗ ████║██╔══██╗████╗  ██║     ]],
+  [[      ██║   ██║       ██╔████╔██║███████║██╔██╗ ██║     ]],
+  [[      ██║   ██║       ██║╚██╔╝██║██╔══██║██║╚██╗██║     ]],
+  [[      ██║   ██║       ██║ ╚═╝ ██║██║  ██║██║ ╚████║     ]],
+  [[      ╚═╝   ╚═╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝     ]],
+  [[                                                        ]],
 }
 dashboard.section.buttons.val = {
   dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -22,7 +24,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "productsway.com"
 end
 
 dashboard.section.footer.val = footer()
