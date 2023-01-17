c4246ceee810cc0e1b54ae8b53b9ae90c4ed11f6
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-17 10:44:15 +0800

    feat: add auto install mason for some languages

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -9,7 +9,23 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "typescript",
+    "javascript",
+    "graphql",
+    "prisma",
+    "tsx",
+    "rust",
+    "go",
+    "toml",
+    "c",
+    "proto",
+  },
+  auto_install = true,
   -- ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
