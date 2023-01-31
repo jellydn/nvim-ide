2f02837ff48d133bb831d613f9b5502172887ec7
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-31 13:04:49 +0800

    feat: add lazy button on dashboard

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -21,6 +21,7 @@ dashboard.section.buttons.val = {
   dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
   dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+  dashboard.button("l", "zZ" .. " Lazy Vim", ":Lazy <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
