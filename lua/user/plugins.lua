a3dbae57d38bf327c5a5919b3f6ba256b83b47bf
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-01 20:38:02 +0800

    fix: lsp for rust

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -129,7 +129,16 @@ return require("lazy").setup {
     -- lazy-load on filetype
     ft = "rust",
     config = function()
-      require("rust-tools").setup {}
+      require("rust-tools").setup {
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
     end,
   },
 
